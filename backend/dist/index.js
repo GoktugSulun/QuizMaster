857dd92d1803ae0591e0730900458aae408b4bca
GoktugSulun <goktug.sulun@hotmail.com>
2024-02-08 11:39:30 +0300

    Connect mongoDB

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 //For env File 
 dotenv.config();
 const app = express();
@@ -12,8 +13,16 @@ app.get('/', (req, res) => {
     res.send('Welcome to Express & TypeScript Server');
 });
 app.get('/test', (req, res) => {
-    res.json("Selam test data :D :)");
+    res.json("Selam test data :D :) :=)");
 });
+try {
+    mongoose.connect(process.env.DB_CONNECTION_URL)
+        .then((res) => console.log('Connected to DB'))
+        .catch((err) => console.log('err => ', err));
+}
+catch (error) {
+    console.log('error => ', error);
+}
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
